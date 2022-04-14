67388e05a999cf61ea906738eebda0ef429f7566
pinguimselvagem <102077133+pinguimselvagem@users.noreply.github.com>
2022-04-14 17:29:09 -0300

    Add files via upload

---
+++ arquivo 5.py
@@ -0,0 +1,12 @@
+
+
+usuario = input("Digite seu login: ")
+print('Usuário Válido')
+senha = input("Digite sua senha: ")      
+print("Cadatro realizado com sucesso!")
+print("Entre com seu usuário e senha!")
+usuario = input("Usuário: ") 
+senha = input("Senha: ") 
+
+if usuario == ' '   and senha == ' ':
+ print("Login realizado")
